7d4bdd8cba983de4a72ad4b80495565cc2404242
Odboy <tianjun@odboy.cn>
2026-05-28 04:09:04 +0000

    update cutego/pkg/logging/index.go. 传递给 Printf 时需要用 v... 展开，否则会把整个切片当作第一个参数
    Signed-off-by: Odboy <tianjun@odboy.cn>

--- cutego/pkg/logging/index.go
+++ cutego/pkg/logging/index.go
@@ -3,25 +3,25 @@ package logging
 import "github.com/druidcaesa/gotool"
 
 func ErrorLog(v ...interface{}) {
-	gotool.Logs.ErrorLog().Println(v)
+	gotool.Logs.ErrorLog().Println(v...)
 }
 
 func ErrorLogf(format string, v ...interface{}) {
-	gotool.Logs.ErrorLog().Printf(format+"\n", v)
+	gotool.Logs.ErrorLog().Printf(format+"\n", v...)
 }
 
 func FatalfLog(format string, v ...interface{}) {
-	gotool.Logs.ErrorLog().Fatalf(format, v)
+	gotool.Logs.ErrorLog().Fatalf(format, v...)
 }
 
 func InfoLog(v ...interface{}) {
-	gotool.Logs.InfoLog().Println(v)
+	gotool.Logs.InfoLog().Println(v...)
 }
 
 func InfoLogf(format string, v ...interface{}) {
-	gotool.Logs.InfoLog().Printf(format+"\n", v)
+	gotool.Logs.InfoLog().Printf(format+"\n", v...)
 }
 
 func DebugLogf(format string, v ...interface{}) {
-	gotool.Logs.DebugLog().Printf(format+"\n", v)
+	gotool.Logs.DebugLog().Printf(format+"\n", v...)
 }
